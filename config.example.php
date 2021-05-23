c9e3ccc29c5e8f2070f6e9f77bafa3ebcaf7c637
Evaldas1994 <evaldas.tuleikis@email.com>
2021-05-23 12:15:03 +0300

    Page load from controller

---
+++ config.example.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DN_NAME', 'mps');
+define('DN_USER', 'root');
+define('DN_PASSWORD', 'root');
+define('DN_HOST', '127.0.0.1');
+
+define('PROJECT_ROOT', __DIR__);
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+include_once '../vendor/autoload.php';
+include_once '../config.php';
+
+use Controller\Index;
+
+$controller = new Index();
+$controller->index();
